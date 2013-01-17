4ed43428773b112563203118cb3b3bbc05a6865f
Lee Hinman <lee@writequit.org>
2013-01-17 16:40:16 -0700

    add one more example

--- src/leiningen/validate.clj
+++ src/leiningen/validate.clj
@@ -77,3 +77,30 @@
    '(do (require 'clojure.tools.namespace.find)
         (require 'clojure.tools.namespace.file)
         (require 'clojure.test))))
+
+;;;;;;;;;;;;;;;;;;;;;;
+
+(defn ^{:no-project-needed true} validate
+  "I test namespaces, unless someone told me to abort"
+  [project & args]
+  (when (some #(= "abort" %) args)
+    (println "Aborting!")
+    (leiningen.core.main/abort))
+  (lein/eval-in-project
+   (-> project
+       (update-in [:dependencies] conj ['org.clojure/tools.namespace "0.2.0"]))
+   `(let [paths# (:test-paths '~project)
+          files# (mapcat
+                  #(-> %
+                       clojure.java.io/file
+                       clojure.tools.namespace.find/find-clojure-sources-in-dir)
+                  paths#)
+          ns-forms# (map clojure.tools.namespace.file/read-file-ns-decl files#)
+          ns-names# (map second ns-forms#)]
+      (doseq [n# ns-names#]
+        (println "Found:" n#)
+        (require n#))
+      (apply clojure.test/run-tests ns-names#))
+   '(do (require 'clojure.tools.namespace.find)
+        (require 'clojure.tools.namespace.file)
+        (require 'clojure.test))))
